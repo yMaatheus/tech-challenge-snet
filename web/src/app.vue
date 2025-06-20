29254271a7cd5d57a028d31eac1739fa2ab48200
Matheus <ymatheus.h@gmail.com>
2025-06-20 15:11:00 -0300

    feat: add vue-sonner for toast notifications and integrate Toaster component

--- web/src/app.vue
+++ web/src/app.vue
@@ -1,4 +1,10 @@
+<script setup lang="ts">
+import { Toaster } from "./components/ui/sonner";
+import 'vue-sonner/style.css'
+</script>
+
 <template>
+  <Toaster class="pointer-events-auto" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
