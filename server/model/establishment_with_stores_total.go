f0fdf14369dcb633c33f2bf23e3e420381cf0657
Matheus <ymatheus.h@gmail.com>
2025-06-20 17:33:28 -0300

    feat: add EstablishmentWithStoresTotal model for establishment management

---
+++ server/model/establishment_with_stores_total.go
@@ -0,0 +1,14 @@
+package model
+
+type EstablishmentWithStoresTotal struct {
+	ID            int64  `json:"id"`
+	Number        string `json:"number"`
+	Name          string `json:"name"`
+	CorporateName string `json:"corporate_name"`
+	Address       string `json:"address"`
+	City          string `json:"city"`
+	State         string `json:"state"`
+	ZipCode       string `json:"zip_code"`
+	AddressNumber string `json:"address_number"`
+	StoresTotal   int    `json:"storesTotal"`
+}
