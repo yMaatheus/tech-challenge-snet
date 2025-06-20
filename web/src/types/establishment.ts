61ef9e301c4c3cbfb64289b98af3ca530ccd1c72
Matheus <ymatheus.h@gmail.com>
2025-06-20 14:33:29 -0300

    feat: add Establishment interface to define establishment data structure

---
+++ web/src/types/establishment.ts
@@ -0,0 +1,10 @@
+export interface Establishment {
+  id: string
+  name: string
+  corporate_name: string
+  address: string
+  address_number: string
+  city: string
+  state: string
+  zip_code: string
+}
